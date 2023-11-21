3016ed9d96ab730b6419429be1fdd092160d34dd
davik20 <onyejivic@gmail.com>
2023-11-21 19:28:17 +0300

    update zupass check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { UserPassportContextProvider } from "../context/PassportContext";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserPassportContextProvider>
+      <Component {...pageProps} />
+    </UserPassportContextProvider>
+  );
 }
