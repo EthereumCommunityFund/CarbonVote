2075b474550227f3fc4c84407ddfd8cdb75fa0b6
MightyDevKnight <james@ecf.network>
2023-11-22 20:35:49 +0100

    Add layout of landingpage

---
+++ components/ui/Button.tsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import { IconType } from 'react-icons';
+import { ImSpinner2 } from 'react-icons/im';
+
+import { cn } from '@/lib/utils';
+
+const ButtonVariant = ['primary'] as const;
+const ButtonSize = ['sm', 'base', 'lg'] as const;
+
+type ButtonProps = {
+  isLoading?: boolean;
+  isDarkBg?: boolean;
+  variant?: (typeof ButtonVariant)[number];
+  size?: (typeof ButtonSize)[number];
+  leftIcon?: IconType;
+  rightIcon?: IconType;
+  classNames?: {
+    leftIcon?: string;
+    rightIcon?: string;
+  };
+} & React.ComponentPropsWithRef<'button'>;
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      className,
+      disabled: buttonDisabled,
+      isLoading,
+      variant = 'primary',
+      size = 'base',
+      isDarkBg = false,
+      leftIcon: LeftIcon,
+      rightIcon: RightIcon,
+      classNames,
+      ...rest
+    },
+    ref
+  ) => {
+    const disabled = isLoading || buttonDisabled;
+
+    return (
+      <button
+        ref={ref}
+        type='button'
+        disabled={disabled}
+        className={cn(
+          'inline-flex items-center font-medium',
+          'focus-visible:ring-primary focus:outline-none focus-visible:ring',
+          'shadow-sm',
+          'transition-colors duration-200',
+          //#region  //*=========== Size ===========
+          [
+            size === 'base' && ['px-3 py-1.5', 'text-sm'],
+            size === 'sm' && ['px-2 py-1', 'text-xs md:text-sm'],
+            size === 'lg' && ['px-4 py-2', 'text-xl font-sans font-semibold md:text-lg'],
+          ],
+          //#endregion  //*======== Size ===========
+          //#region  //*=========== Variants ===========
+          [
+            variant === 'primary' && [
+              'bg-btnPrimary/20 text-[#FFFFFF]',
+              'border',
+              'hover:bg-btnPrimary/30',
+            ],
+          ],
+          //#endregion  //*======== Variants ===========
+          'disabled:cursor-not-allowed',
+          isLoading &&
+          'relative text-transparent transition-none hover:text-transparent disabled:cursor-wait',
+          className
+        )}
+        {...rest}
+      >
+        {isLoading && (
+          <div
+            className={cn(
+              'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2',
+              {
+                'text-white': ['primary', 'dark'].includes(variant),
+                'text-black': ['light'].includes(variant),
+                'text-primary-500': ['outline', 'ghost'].includes(variant),
+              }
+            )}
+          >
+            <ImSpinner2 className='animate-spin' />
+          </div>
+        )}
+        {LeftIcon && (
+          <div
+            className={cn([
+              size === 'base' && 'mr-1',
+              size === 'sm' && 'mr-1.5',
+              size === 'lg' && 'mr-2',
+            ])}
+          >
+            <LeftIcon
+              size='1em'
+              className={cn(
+                [
+                  size === 'base' && 'md:text-md text-md',
+                  size === 'sm' && 'md:text-md text-sm',
+                ],
+                classNames?.leftIcon
+              )}
+            />
+          </div>
+        )}
+
+        {children}
+        {RightIcon && (
+          <div
+            className={cn([
+              size === 'base' && 'ml-1',
+              size === 'sm' && 'ml-1.5',
+              size === 'lg' && 'ml-2',
+            ])}
+          >
+            <RightIcon
+              size='1.25em'
+              className={cn(
+                [
+                  size === 'base' && 'text-md md:text-md',
+                  size === 'sm' && 'md:text-md text-sm',
+                ],
+                classNames?.rightIcon
+              )}
+            />
+          </div>
+        )}
+      </button>
+    );
+  }
+);
+
+export default Button;
