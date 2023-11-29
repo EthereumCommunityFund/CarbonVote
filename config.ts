20f43ccf4be2581c87671bf2ec03c657ea64bf73
davik20 <onyejivic@gmail.com>
2023-11-29 13:24:49 +0300

    update

---
+++ config.ts
@@ -0,0 +1,40 @@
+
+const ticketTypes: Record<TicketTypeName, Array<{ eventId: string; productId: string }>> = {
+    ZuzaluResident: [
+        {
+            eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
+            productId: "5ba4cd9e-893c-4a4a-b15b-cf36ceda1938"
+        }
+    ],
+    ZuzaluOrganizer: [
+        {
+            eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
+            productId: "10016d35-40df-4033-a171-7d661ebaccaa"
+        }
+    ],
+    ZuzaluVisitor: [
+        {
+            eventId: "5de90d09-22db-40ca-b3ae-d934573def8b",
+            productId: "53b518ed-e427-4a23-bf36-a6e1e2764256"
+        }
+    ],
+    ZuConnectResident: [
+        {
+            eventId: "91312aa1-5f74-4264-bdeb-f4a3ddb8670c",
+            productId: "cc9e3650-c29b-4629-b275-6b34fc70b2f9"
+        },
+        {
+            eventId: "54863995-10c4-46e4-9342-75e48b68d307",
+            productId: "d2123bf9-c027-4851-b52c-d8b73fc3f5af"
+        },
+        {
+            eventId: "797de414-2aec-4ef8-8655-09df7e2b6cc6",
+            productId: "d3620f38-56a9-4235-bea8-0d1dba6bb623"
+        },
+        {
+            eventId: "a6109324-7ca0-4198-9583-77962d1b9d53",
+            productId: "a6109324-7ca0-4198-9583-77962d1b9d53"
+        }
+    ]
+}
+
--- context/PassportContext.tsx
+++ context/PassportContext.tsx
@@ -6,9 +6,12 @@ import {
   useEffect,
 } from "react";
 
+import { useZuAuth, supportedEvents, supportedProducs } from "zuauth";
+
 // ("@pcd/passport-interface");
 
 import { useZupassPopupMessages } from "@pcd/passport-interface/src/PassportPopup";
+import { EdDSATicketFieldsToReveal } from "@pcd/zk-eddsa-event-ticket-pcd";
 import {
   PASSPORT_SERVER_URL,
   ZUPASS_SERVER_URL,
@@ -28,6 +31,8 @@ import { fetchAllPolls } from "../controllers/poll.controller";
 
 type UserPassportContextData = {
   signIn: any;
+  isAuthenticated: boolean;
+  verifyZupassTicket: any;
   // user: User | null
 };
 
@@ -37,19 +42,43 @@ type UserPassportProviderProps = {
 
 export const UserPassportContext = createContext({} as UserPassportContextData);
 
+const defaultSetOfTicketFieldsToReveal: EdDSATicketFieldsToReveal = {
+  revealTicketId: true,
+  revealEventId: true,
+  revealProductId: true,
+  revealTimestampConsumed: true,
+  revealTimestampSigned: true,
+  revealAttendeeSemaphoreId: true,
+  revealIsConsumed: true,
+  revealIsRevoked: true,
+  revealTicketCategory: false,
+  revealAttendeeEmail: true,
+  revealAttendeeName: true,
+};
+
 export function UserPassportContextProvider({
   children,
 }: UserPassportProviderProps) {
   const router = useRouter();
+
+  console.log(supportedEvents, supportedProducs);
+  const { authenticate, pcd } = useZuAuth();
+  const [ticketFieldsToReveal, setTicketFieldsToReveal] =
+    useState<EdDSATicketFieldsToReveal>(defaultSetOfTicketFieldsToReveal);
+
+  console.log(pcd);
+
   const [pcdStr] = useZupassPopupMessages();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   useEffect(() => {
     if (pcdStr) {
       let pcd = JSON.parse(pcdStr);
-
       let _pcd = JSON.parse(pcd.pcd);
       let res = verifyProof(_pcd);
     }
   }, [pcdStr]);
+
   const signIn = () => {
     openGroupMembershipPopup(
       "https://zupass.org/",
@@ -59,10 +88,65 @@ export function UserPassportContextProvider({
       undefined,
       undefined
     );
+    setIsAuthenticated(true);
   };
 
+  // fieldsToReveal: EdDSATicketFieldsToReveal,
+  // watermark: string | bigint,
+  // externalNullifier?: string | bigint,
+  // validEventIds: string[] = supportedEvents,
+  // validProductIds: string[] = supportedProducs,
+  // popupRoute: string = "popup"
+  const verifyZupassTicket = () => {
+    authenticate(
+      ticketFieldsToReveal,
+      "1366567502",
+      undefined,
+      [
+        "5de90d09-22db-40ca-b3ae-d934573def8b",
+        "5de90d09-22db-40ca-b3ae-d934573def8b",
+        "5de90d09-22db-40ca-b3ae-d934573def8b",
+        "91312aa1-5f74-4264-bdeb-f4a3ddb8670c",
+        "54863995-10c4-46e4-9342-75e48b68d307",
+        "797de414-2aec-4ef8-8655-09df7e2b6cc6",
+        "a6109324-7ca0-4198-9583-77962d1b9d53",
+      ],
+      [
+        "5ba4cd9e-893c-4a4a-b15b-cf36ceda1938",
+        "10016d35-40df-4033-a171-7d661ebaccaa",
+        "53b518ed-e427-4a23-bf36-a6e1e2764256",
+        "cc9e3650-c29b-4629-b275-6b34fc70b2f9",
+        "d2123bf9-c027-4851-b52c-d8b73fc3f5af",
+        "d3620f38-56a9-4235-bea8-0d1dba6bb623",
+        "a6109324-7ca0-4198-9583-77962d1b9d53",
+      ],
+      "ticket-popup"
+    );
+  };
+
+  //   ZuConnectResident: [
+  //     {
+  //         eventId: "91312aa1-5f74-4264-bdeb-f4a3ddb8670c",
+  //         productId: "cc9e3650-c29b-4629-b275-6b34fc70b2f9"
+  //     },
+  //     {
+  //         eventId: "54863995-10c4-46e4-9342-75e48b68d307",
+  //         productId: "d2123bf9-c027-4851-b52c-d8b73fc3f5af"
+  //     },
+  //     {
+  //         eventId: "797de414-2aec-4ef8-8655-09df7e2b6cc6",
+  //         productId: "d3620f38-56a9-4235-bea8-0d1dba6bb623"
+  //     },
+  //     {
+  //         eventId: "a6109324-7ca0-4198-9583-77962d1b9d53",
+  //         productId: "a6109324-7ca0-4198-9583-77962d1b9d53"
+  //     }
+  // ]
+
   return (
-    <UserPassportContext.Provider value={{ signIn }}>
+    <UserPassportContext.Provider
+      value={{ signIn, isAuthenticated, verifyZupassTicket }}
+    >
       {children}
     </UserPassportContext.Provider>
   );
