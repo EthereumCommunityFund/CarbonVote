75d7bb3adc7e8e614fbcd9e7556a8ceb224a60a2
dev_waliba <donwaleyb@gmail.com>
2023-11-30 15:50:42 +0100

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,16 +2,20 @@ require('dotenv').config({ path: './.env.local' });
 
 require('@nomiclabs/hardhat-ethers');
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { GOERLI_API_URL, API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.19',
-  defaultNetwork: 'volta',
+  defaultNetwork: 'goerli',
   networks: {
     hardhat: {},
     volta: {
       url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: GOERLI_API_URL, // Your Goerli API URL
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
