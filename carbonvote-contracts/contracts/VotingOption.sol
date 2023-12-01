dabc6c0b2f1b18f70047c0e3100bee2285389240
davik20 <onyejivic@gmail.com>
2023-12-02 02:06:52 +0300

    update remove voters from array

--- carbonvote-contracts/contracts/VotingOption.sol
+++ carbonvote-contracts/contracts/VotingOption.sol
@@ -27,33 +27,43 @@ contract VotingOption {
         option_index = _option_index;
     }
 
-    receive() external payable {}
+    fallback() external payable {}
 
-    fallback() external payable {
+    receive() external payable {
         require(msg.value == 0, "Cannot send ETH with vote");
-        (
-            string memory message,
-            bytes memory signature
-        ) = decodeMessageAndSignature(msg.data);
-
-        castVote(signature, message);
+        castTransactionVote(msg.sender);
     }
 
-    function castVote(bytes memory signature, string memory message) public {
+    function castVote(
+        address voter,
+        bytes memory signature,
+        string memory message
+    ) public {
         require(block.timestamp < endTime, "Poll has ended");
         VotingContract(mainContract).verifyAndRecordVote(
-            msg.sender,
+            voter,
             pollIndex,
             option_index,
             signature,
             message
         );
 
-        if (!hasVoted[msg.sender]) {
-            voters.push(msg.sender);
-            voterIndex[msg.sender] = voters.length - 1;
+        if (!hasVoted[voter]) {
+            voters.push(voter);
+            voterIndex[voter] = voters.length - 1;
         }
-        hasVoted[msg.sender] = true;
+        hasVoted[voter] = true;
+    }
+
+    function castTransactionVote(address voter) public {
+        require(block.timestamp < endTime, "Poll has ended");
+        VotingContract(mainContract).recordVote(voter, pollIndex, option_index);
+
+        if (!hasVoted[voter]) {
+            voters.push(voter);
+            voterIndex[voter] = voters.length - 1;
+        }
+        hasVoted[voter] = true;
     }
 
     function removeVote(address voter) public {
@@ -75,8 +85,9 @@ contract VotingOption {
         delete voterIndex[voter];
         hasVoted[voter] = false;
     }
+
     function getVotersCount() public view returns (uint256) {
-    return voters.length;
+        return voters.length;
     }
 
     function decodeMessageAndSignature(
