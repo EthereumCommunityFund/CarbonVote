3d76fa22e22fee45f0b28f0201cd7d2322f74c08
davik20 <onyejivic@gmail.com>
2023-12-02 15:57:18 +0300

    update

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -5,3 +5,6 @@ import axiosInstance from "../src/axiosInstance"
 export const verifyProof = async (pcd: string) => {
     return await axiosInstance.post('/api/auth/verify', { pcd })
 }
+export const generate_signature = async () => {
+    return await axiosInstance.post('/api/auth/generate_signature')
+}
