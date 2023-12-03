dec35bcc0537dfdc745645301b0c5f29b641a12f
davik20 <onyejivic@gmail.com>
2023-12-03 16:00:34 +0300

    make contracts upgradable

--- carbonvote_contracts/hardhat.config.ts
+++ carbonvote_contracts/hardhat.config.ts
@@ -1,12 +1,19 @@
 require('dotenv').config({ path: './.env.local' });
 
 require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
 
 // const { GOERLI_API_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.19',
+  solidity: {
+    compilers: [{ version: "0.8.0" },
+    { version: "0.8.1" },
+    { version: "0.8.20" },
+      // ... add more versions as needed
+    ],
+  },
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
