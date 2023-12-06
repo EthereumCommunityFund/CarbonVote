62e35cf11d2638efd4de84958a1376d508340f08
dev_waliba <donwaleyb@gmail.com>
2023-12-06 23:54:22 +0100

    use toast

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -21,7 +21,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: 'border bg-background text-foreground',
+        default: 'border bg-light text-foreground',
         destructive: 'destructive group border-destructive bg-destructive text-destructive-foreground',
       },
     },
