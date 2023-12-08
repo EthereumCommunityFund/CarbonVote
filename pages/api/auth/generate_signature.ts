2dc05e075cd1769a96bbb7ef8680f261a4431d29
davik20 <onyejivic@gmail.com>
2023-12-08 14:45:15 +0100

    update

--- pages/api/auth/generate_signature.ts
+++ pages/api/auth/generate_signature.ts
@@ -19,8 +19,11 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     let privateKey = process.env.PRIVATE_KEY as string;
     let message = req.body.message
 
+
+
     if (!message) return res.status(400).send("No message found in req.body")
     let signed_message = await signMessage(privateKey, req.body.message);
+    console.log('message', message, signed_message)
     console.log(signed_message);
     res.status(200).send({ data: { signed_message, message } });
 }
