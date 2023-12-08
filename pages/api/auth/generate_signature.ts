a815237a33d59015716f7cf823245b311740404d
davik20 <onyejivic@gmail.com>
2023-12-08 14:11:10 +0100

    update generate signature

--- pages/api/auth/generate_signature.ts
+++ pages/api/auth/generate_signature.ts
@@ -13,22 +13,16 @@ function hash_message(message: string) {
     return ethers.keccak256(ethers.toUtf8Bytes(message))
 }
 
-function prefixedHash(message: string) {
-    let messageHash = ethers.keccak256(ethers.toUtf8Bytes(message));
-    return ethers.keccak256(ethers.toUtf8Bytes("\x19Ethereum Signed Message:\n32", messageHash));
-    // keccak256(
-    //     abi.encodePacked(
-    //         "\x19Ethereum Signed Message:\n32",
-    //         messageHash
-    //     )
-    // );
-}
+
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     let privateKey = process.env.PRIVATE_KEY as string;
-    let signed_message = await signMessage(privateKey, "message");
+    let message = req.body.message
+
+    if (!message) return res.status(400).send("No message found in req.body")
+    let signed_message = await signMessage(privateKey, req.body.message);
     console.log(signed_message);
-    res.status(200).send({ data: signed_message });
+    res.status(200).send({ data: { signed_message, message } });
 }
 
 export default handler;
