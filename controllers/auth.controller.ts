b3d09f869f6ef8ea33b71bf03342e0ad3184e7dc
davik20 <onyejivic@gmail.com>
2023-12-08 14:11:10 +0100

    update generate signature

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -5,6 +5,6 @@ import axiosInstance from "../src/axiosInstance"
 export const verifyProof = async (pcd: string) => {
     return await axiosInstance.post('/api/auth/verify', { pcd })
 }
-export const generate_signature = async () => {
-    return await axiosInstance.post('/api/auth/generate_signature')
+export const generate_signature = async (message: string) => {
+    return await axiosInstance.post('/api/auth/generate_signature', { message })
 }
