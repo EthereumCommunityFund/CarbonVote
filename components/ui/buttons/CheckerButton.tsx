83770a56864a07ae603855caaf4902d77c922ba6
MightyDevKnight <james.mario.bello1209@gmail.com>
2023-12-14 03:42:46 -0600

    update poll option editable

--- components/ui/buttons/CheckerButton.tsx
+++ components/ui/buttons/CheckerButton.tsx
@@ -9,20 +9,30 @@ interface CheckerButtonProps {
 
 const CheckerButton: React.FC<CheckerButtonProps> = ({ option, onCheckboxChange }) => {
   const [isChecked, setIsChecked] = useState<boolean>(option.isChecked);
+  const [inputValue, setInputValue] = useState<string>(option.name);
+
 
   const handleCheckboxChange = () => {
     const newCheckedValue = !isChecked;
     setIsChecked(newCheckedValue);
-    onCheckboxChange(option.name, newCheckedValue); // Notify parent of the change
+    onCheckboxChange(inputValue, newCheckedValue); // Notify parent of the change
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
   };
 
   return (
-    <label className="flex items-center space-x-2 cursor-pointer">
+    <label className="flex items-center space-x-2 cursor-pointer" onClick={handleCheckboxChange}>
       <span className={`text-lg font-bold ${isChecked ? 'text-blue-500' : 'text-gray-700'}`}>{isChecked ? '✔️' : ''}</span>
       <span className={`w-full rounded-full relative transition-all duration-300 items-center px-4 py-2 ${isChecked ? 'bg-blue-500' : 'bg-white'}`}>
-        <Label>{option.name}</Label>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          className="w-full h-full border-none outline-none bg-transparent cursor-pointer"
+        />
       </span>
-      <input type="checkbox" className="hidden" checked={isChecked} onChange={handleCheckboxChange} />
     </label>
   );
 };
