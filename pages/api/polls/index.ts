18f75449b63452cc1cad87671bf21167f43531be
davik20 <onyejivic@gmail.com>
2023-12-17 21:46:04 +0100

    add create and view poll

--- pages/api/polls/index.ts
+++ pages/api/polls/index.ts
@@ -1,17 +1,62 @@
-import { NextApiHandler } from "next";
-import { NextApiRequest, NextApiResponse } from "next";
-import withAuthorization from "../middlware/withAuthorization";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from '../../../utils/supabaseClient';
 
+const viewAllPolls = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method !== 'GET') {
+        res.status(405).send('Method Not Allowed');
+        return;
+    }
 
+    try {
+        // Fetch all polls
+        let { data: polls, error: pollsError } = await supabase
+            .from('polls')
+            .select('*');
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log('reached handler')
-}
+        if (pollsError) throw pollsError;
 
-export default withAuthorization(handler);
+        // Fetch options and credentials for each poll
+        for (let poll of polls as any) {
+            let { data: options, error: optionsError } = await supabase
+                .from('options')
+                .select('*')
+                .eq('poll_id', poll.id);
 
+            if (optionsError) throw optionsError;
 
+            // Fetch credential details for each poll
+            let { data: credentials, error: credentialsError } = await supabase
+                .from('pollcredentials')
+                .select(`
+                 credential_id,
+                 credentials!inner (
+                     credential_name,
+                     credential_type,
+                     credential_detail
+                 )
+             `)
+                .eq('poll_id', poll.id);
 
+            if (credentialsError) throw credentialsError;
 
+            // Merge options and credential details into the poll object
+            poll.options = options;
+            if (credentials) {
+                poll.credentials = credentials.map(cred => ({
+                    id: cred.credential_id,
+                    ...cred.credentials
+                }));
 
+            }
 
+        }
+
+        // Send response with all polls and their associated data
+        res.status(200).json(polls);
+    } catch (error: any) {
+        // Handle errors
+        res.status(500).json({ error: error.message || 'An unexpected error occurred' });
+    }
+};
+
+export default viewAllPolls;
--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,10 +1,13 @@
 // utils/supabaseClient.js
 
 import { createClient } from '@supabase/supabase-js';
-import { Database } from 'lucide-react';
+
+import {Database} from '../database.types'
+
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
