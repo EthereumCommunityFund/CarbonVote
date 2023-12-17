fd2aa06d2eaadc76acbb4f6c6cf2fa02164e4b8d
davik20 <onyejivic@gmail.com>
2023-12-17 23:07:07 +0100

    add vote

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: false,
   transpilePackages: ["@pcd/passport-interface"],
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-    };
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        checkResource: (resource, context) => {
+          if (context.includes(path.resolve(__dirname, './carbonvote-contracts'))) {
+            return true; 
+          }
+          return false; 
+        },
+      })
+    );
+
+    config.resolve.fallback = { fs: false };
+
     return config;
   },
 };
