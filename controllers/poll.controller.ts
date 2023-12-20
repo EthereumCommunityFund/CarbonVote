937f1d114b60c9054de746f376fe600fc425451f
davik20 <onyejivic@gmail.com>
2023-12-20 11:29:06 +0100

    add create to controller

--- controllers/poll.controller.ts
+++ controllers/poll.controller.ts
@@ -1,36 +1,36 @@
 import axiosInstance from '../src/axiosInstance';
 
 export type PollRequestData = {
-  title: string;
-  description: string;
-  time_limit: number;
-  voting_method: string;
-  options: OptionData[];
-  credentials: string[];
+    title: string;
+    description: string;
+    time_limit: number;
+    voting_method: string;
+    options: OptionData[];
+    credentials: string[];
 };
 
 export type VoteRequestData = {
-  option_id: string;
-  voter_identifier: string;
-  poll_id: string;
+    option_id: string;
+    voter_identifier: string;
+    poll_id: string;
 };
 
 export type OptionData = {
-  description: string;
+    description: string;
 };
 
 export const fetchAllPolls = async () => {
-  return await axiosInstance.get('/api/polls');
+    return await axiosInstance.get('/api/polls');
 };
 
 export const fetchPollById = async (pollId: string) => {
-  return await axiosInstance.get(`/api/polls/${pollId}`);
+    return await axiosInstance.get(`/api/polls/${pollId}`);
 };
 
 export const createPoll = async (pollData: PollRequestData) => {
-  return await axiosInstance.post('/api/polls', pollData);
+    return await axiosInstance.post('/api/polls/create', pollData);
 };
 
 export const castVote = async (voteData: VoteRequestData) => {
-  return await axiosInstance.post('/api/vote', voteData);
+    return await axiosInstance.post('/api/vote', voteData);
 };
