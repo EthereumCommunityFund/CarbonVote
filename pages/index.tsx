7746c8f905e2904cc830dae024d095487e5789f3
wentianyang66 <wentianyang66@gmail.com>
2024-01-06 20:14:49 +0100

    Fix deploy issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,8 @@ export default function Home() {
   const router = useRouter();
   const { signIn, isPassportConnected } = useUserPassportContext();
   const contractAbi = VotingContract.abi;
-  const contractAddress = contract_addresses.VotingContract;
+  //const contractAddress = contract_addresses.VotingContract;
+  const contractAddress = "0x12B4b94e5d5D0a2433851f9B423CC0B5a4C71DEa";
 
   const fetchPollsFromContract = async () => {
     if (window.ethereum) {
