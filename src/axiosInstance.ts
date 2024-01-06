4df6026bfcc21b53ef3bb01504ec5219b20aa8a9
wentianyang66 <wentianyang66@gmail.com>
2024-01-06 21:03:38 +0100

    Fix deploy issue

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -5,7 +5,7 @@ const axiosInstance: AxiosInstance = axios.create({
   // baseURL: 'https://zuzapp-test-v1-events-app-delta.vercel.app', // replace with your API endpoin
   // baseURL: 'https://www.zuzalu.city',
   //baseURL: 'http://localhost:3000',
-  baseURL: 'https://testcarbonvote-oi1pbs2jb-testcarbonvote.vercel.app',
+  baseURL: 'https://testcarbonvote-git-staging-testcarbonvote.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
