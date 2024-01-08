2f2c7212809a994e0ceca1fb1d763754d9f20110
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-01-08 01:52:30 -0600

    Add pie chart

---
+++ components/ui/PieChart.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+
+const PieChart: React.FC = () => {
+  // Data for the pie chart
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h2>Pie Chart Example</h2>
+      <Pie data={data} />
+    </div>
+  );
+};
+
+export default PieChart;
