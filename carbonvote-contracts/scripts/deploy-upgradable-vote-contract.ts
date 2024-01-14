d003d6977b97f9572b5268752a0f26ed0614201f
0xpetra <franco.petraz@gmail.com>
2024-01-14 13:48:46 -0300

    🐛 FIX: deploy fc

--- carbonvote-contracts/scripts/deploy-upgradable-vote-contract.ts
+++
@@ -1,9 +0,0 @@
-const { upgrades } = require('hardhat');
-
-async function main(): Promise<void> {
-  const Box = await ethers.getContractFactory('VotingContract');
-  console.log('Deploying Voting Contract');
-  const votingContract = await upgrades.deployProxy(Box, [42], { initializer: 'store' });
-  await votingContract.deployed();
-  console.log('Box deployed to:', votingContract.address);
-}
