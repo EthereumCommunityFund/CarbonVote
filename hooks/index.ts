7ac9bcbf5e783385b45fb013e3a1e106861a79d2
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-01-24 14:14:29 -0600

    update url to sepolia

--- hooks/index.ts
+++ hooks/index.ts
@@ -3,10 +3,10 @@ import axios from 'axios';
 
 const fetchAccountTransactions = async (accountAddress: string) => {
   const apiKey = process.env.NEXT_PUBLIC_ETHERSCAN_KEY;
-  const apiUrl = `https://api.etherscan.io/api?module=account&action=txlist&address=${accountAddress}&startblock=0&endblock=99999999&sort=desc&apikey=${apiKey}`;
+  const apiUrl = `https://api-sepolia.etherscan.io/api?module=account&action=txlist&address=${accountAddress}&startblock=0&endblock=99999999&sort=desc&apikey=${apiKey}`;
 
   const response = await axios.get(apiUrl);
-  console.log(response, accountAddress, 'response: ')
+
   return response.data.result;
 };
 
