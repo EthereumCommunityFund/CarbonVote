da433f8349b020b4af2963801607847d37a6e9f0
wentianyang66 <wentianyang66@gmail.com>
2024-01-24 01:20:43 +0100

    change option button ui

--- components/ui/buttons/OptionButton.tsx
+++ components/ui/buttons/OptionButton.tsx
@@ -21,15 +21,17 @@ const OptionButton: React.FC<OptionButtonProps> = ({ optionName, onVote, isCheck
   };
 
   return (
-    <label className="flex items-center space-x-2 cursor-pointer">
-      <span className={`text-lg font-bold ${isChecked ? 'text-blue-500' : 'text-gray-700'}`}>{isChecked ? '✔️' : ''}</span>
-      <span onClick={handleVote} className={`w-full rounded-full relative transition-all duration-300 items-center px-4 py-2 flex ${isChecked ? 'bg-blue-500' : 'bg-white'} ${optionName === 'No' ? 'text-[#FF6384]' : 'text-[#00FF00]'}`}>
-        <Label>{optionName}: </Label>
-        <div className='w-full flex justify-center'>
-          <Label>{optionAddress}</Label>
-        </div>
-      </span>
-      <input type="checkbox" className="hidden" checked={isChecked} readOnly />
+    <label className="flex flex-col items-start space-y-1 cursor-pointer w-full"> {/* 修改了这里 */}
+      <div className="flex items-center space-x-2 w-full">
+        <span className={`text-lg font-bold ${isChecked ? 'text-blue-500' : 'text-gray-700'}`}>{isChecked ? '✔️' : ''}</span>
+        <span onClick={handleVote} className={`rounded-full relative transition-all duration-300 items-center px-4 py-3 flex ${isChecked ? 'bg-blue-500' : 'bg-white'} ${optionName === 'No' ? 'text-[#FF6384]' : 'text-[#00FF00]'} w-full`}>
+          <Label className="w-full">{optionName}: </Label>
+        </span>
+        <input type="checkbox" className="hidden" checked={isChecked} readOnly />
+      </div>
+      <div className='w-full flex justify-left px-4 py-2'>
+        <span>Address: {optionAddress}</span>
+      </div>
     </label>
   );
 };
