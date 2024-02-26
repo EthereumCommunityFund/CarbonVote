054c8031b1b352e161bd3823fd64949cb37d64d9
MightyDevKnight <james.mario.bello1209@gmail.com>
2024-02-26 11:03:09 +0100

    Add markdown text editor

--- components/ui/TextEditor.tsx
+++ components/ui/TextEditor.tsx
@@ -1,6 +1,5 @@
-import dynamic from "next/dynamic";
-
-const ReactQuillNoSSR = dynamic(() => import('react-quill'), { ssr: false });
+import SimpleMdeReact from 'react-simplemde-editor';
+import "easymde/dist/easymde.min.css";
 
 interface TextEditorProps {
   value: string;
@@ -8,32 +7,9 @@ interface TextEditorProps {
 }
 
 const TextEditor: React.FC<TextEditorProps> = ({ value, onChange }) => {
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [{ size: [] }],
-      [{ font: [] }],
-      [{ align: ["right", "center", "justify"] }],
-      [{ list: "ordered" }, { list: "bullet" }],
-      ["link", "image"],
-      [{ color: ["red", "#785412"] }],
-      [{ background: ["red", "#785412"] }]
-    ]
-  }
 
-  const formats = [
-    "header", "bold", "italic", "underline", "strike", "blockquote", "list", "bullet", "link", "color", "image", "background", "align", "size", "font"
-  ]
   return (
-    <ReactQuillNoSSR
-      className={"w-full flex flex-col gap-2"}
-      placeholder={"Enter Description"}
-      value={value}
-      onChange={onChange}
-      modules={modules}
-      formats={formats}
-    />
+    <SimpleMdeReact className='w-[700px]' value={value} onChange={onChange} />
   );
 };
 
